7e97224c716859679e29019df293e2d10d936a69
trungdao912 <trungdao912@gmail.com>
2019-01-17 10:59:28 +0700

    update

--- Session10/javascript-async-app/js/main.js
+++ Session10/javascript-async-app/js/main.js
@@ -24,6 +24,34 @@
   }
 
   // Promise
+  // fetch('https://jsonplaceholder.typicode.com/users')
+  //     .then((users) => {
+  //       return users.json();
+  //     })
+  //     .then((users) => {
+  //       users.forEach((user) => {
+  //         fetch(`https://jsonplaceholder.typicode.com/posts?userId=${user.id}`)
+  //           .then((posts) => {
+  //             return posts.json();
+  //           })
+  //           .then((posts) => {
+  //             user.posts = posts;
+  //             posts.forEach((post) => {
+  //               fetch(`https://jsonplaceholder.typicode.com/comments?postId=${post.id}`)
+  //                 .then((comments) => {
+  //                   return comments.json();
+  //                 })
+  //                 .then((comments) => {
+  //                   post.comments = comments;
+  //                   $scope.$apply(function() {
+  //                     $scope.users = users;
+  //                 });                  
+  //                 })
+  //             })
+
+  //           })
+  //       })
+  //     })
   
 })();
 
